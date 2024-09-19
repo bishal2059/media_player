06c9a065150b8c93710a00c7c333a5326daa3b84
bishal2059 <bishal.sap21@gmail.com>
2024-09-19 23:47:01 +0545

    volume changes added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,13 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    player = new QMediaPlayer();
+
+    ui->pushButton_pause_play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->pushButton_stop->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
+    ui->pushButton_seek_backward->setIcon(style()->standardIcon(QStyle::SP_MediaSeekBackward));
+    ui->pushButton_seek_forward->setIcon(style()->standardIcon(QStyle::SP_MediaSeekForward));
+    ui->pushButton_volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
 }
 
 MainWindow::~MainWindow()
@@ -53,6 +60,15 @@ void MainWindow::on_pushButton_seek_forward_clicked()
 void MainWindow::on_pushButton_volume_clicked()
 {
 
+    if(Is_Muted == False){
+        Is_Muted =true;
+        ui->pushButton_volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolumeMuted));
+        player->setMuted(true);
+    }else{
+        Is_Muted =false;
+        ui->pushButton_volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
+        player->setMuted(false);
+    }
 }
 
 
